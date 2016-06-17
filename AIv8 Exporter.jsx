5b9a385f8f6cae69ce1fb6bbae22903b5416b7a4
Christian Wilcox <cw@christianwilcox.com>
2016-06-17 18:20:25 -0500

    added save as legacy AI v8 code; set folder to active doc path

--- AIv8 Exporter.jsx
+++ AIv8 Exporter.jsx
@@ -29,7 +29,12 @@ var exportFolder,
     sourceDoc,
     itemsToExport,
     exportDoc,
-    svgOptions;
+    svgOptions,
+    overwrite = true, // boolean
+    // Accepted values:
+    // 8, 9, 10, 11 (cs), 12 (cs2), 13 (cs3), 14 (cs4), 15 (cs5), 16 (cs6), 17 (cc)
+    targetVersion = 8;
+
 
 try {
   if ( app.documents.length > 0 ) {
@@ -42,7 +47,8 @@ try {
 
     itemsToExport = [];
     sourceDoc = app.activeDocument;
-    exportFolder = Folder.selectDialog('Select Folder to Save Files');
+    # set to same folder as active document
+    exportFolder =  app.activeDocument.path;
     exportDoc = documents.add(DocumentColorSpace.RGB);
 
     main();
@@ -78,6 +84,21 @@ function main() {
     // Empty export document
     exportDoc.pageItems.removeAll();
   }
+
+}
+
+function SaveOptions_ai() {
+
+    var saveOptions = new IllustratorSaveOptions();
+
+    saveOptions.compatibility = Compatibility[ "ILLUSTRATOR" + targetVersion ];
+    saveOptions.flattenOutput = OutputFlattening.PRESERVEAPPEARANCE;
+    saveOptions.compressed = true; // Version 10 or later
+    saveOptions.pdfCompatible = true; // Version 10 or later
+    saveOptions.embedICCProfile = true; // Version 9 or later
+    saveOptions.embedLinkedFiles = false; // Version 7 or later
+
+    return saveOptions
 
 }
 
@@ -227,7 +248,11 @@ function exportSVG(doc, name, bounds, exportOptions) {
   doc.artboards[0].artboardRect = bounds;
 
   var file = new File( exportFolder.fsName + '/' + name );
-  doc.exportFile( file, ExportType.SVG, exportOptions );
+  //doc.exportFile( file, ExportType.SVG, exportOptions );
+
+  // Save
+  app.activeDocument.saveAs( file, SaveOptions_ai() )
+
 }
 
 function getNamedItems(doc) {
